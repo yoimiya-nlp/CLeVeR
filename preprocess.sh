a7d145b12534036a7e00d6fceed5a1399a194f71
Jiayuan Li <13700017086@163.com>
2025-08-17 11:25:11 +0800

    update config

--- preprocess.sh
+++ preprocess.sh
@@ -6,6 +6,6 @@ python data_preprocess.py \
     --pretrain_text_model_name=pretrain_text_model \
     --pretrain_code_model_name=pretrain_code_model \
     --program_language=c \
-    --dataset=dataset/vcldata.jsonl \
-    --dataset_name=vcldata \
+    --dataset=dataset/dataset.jsonl \
+    --dataset_name=dataset \
     --seed 123456
